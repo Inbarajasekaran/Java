745dce394e472166b227a1be116b706b5b7d7c83
Inbarajasekaran <inbarajasekaran0011@gmail.com>
2022-04-17 14:23:30 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String myName = "Inba rajasekaran";
+        System.out.println("Hello " + myName + "...! Welcome to Java");
+    }
+}
