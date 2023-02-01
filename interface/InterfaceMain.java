4aedba07fb23e0f9aca8ff557b8c1b83bebd26cd
Inba <inba@movingwalls.com>
2023-02-01 13:04:16 +0530

    change main method in interface

--- interface/InterfaceMain.java
+++ interface/InterfaceMain.java
@@ -1,4 +1,4 @@
-public class Main {
+public class InterfaceMain {
     public static void main(String[] args) {
         // InterfaceClass interfaceClass = new InterfaceClass();
         // // interfaceClass.add();
